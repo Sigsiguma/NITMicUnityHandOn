ee10cb2a85334bb7fd5906ab1169c9938de7be67
Siguma <you8jun5@gmail.com>
2016-11-27 22:35:32 +0900

    UnityProjectも追加

---
+++ UnityProject/UnityTutorial/Assets/Scripts/InGame/Coin/CoinGenerator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinGenerator : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject coin_prefab_;
+
+    //public GameObject coin_prefab_; でもよいが、上を推奨
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.I)) {
+            Instantiate(coin_prefab_, RandomCoinPos(), Quaternion.identity);
+        }
+    }
+
+    //コインの生成位置をランダムに返す
+    private Vector3 RandomCoinPos() {
+        int pos_x = Random.Range(-90, 90);
+        int pos_z = Random.Range(-90, 90);
+
+        Vector3 pos = new Vector3(pos_x, 5, pos_z);
+        return pos;
+    }
+
+}
---
+++ UnityProject/UnityTutorial/Assets/Scripts/InGame/Player/MovePlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour {
+
+    private const float move_speed_ = 0.2f;
+
+    private void Update() {
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            Vector3 pos = transform.position;
+            pos.x -= move_speed_;
+            transform.position = pos;
+        } else if (Input.GetKey(KeyCode.RightArrow)) {
+            Vector3 pos = transform.position;
+            pos.x += move_speed_;
+            transform.position = pos;
+        } else if (Input.GetKey(KeyCode.DownArrow)) {
+            Vector3 pos = transform.position;
+            pos.z -= move_speed_;
+            transform.position = pos;
+        } else if (Input.GetKey(KeyCode.UpArrow)) {
+            Vector3 pos = transform.position;
+            pos.z += move_speed_;
+            transform.position = pos;
+        }
+    }
+}
